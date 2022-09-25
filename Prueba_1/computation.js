ebd1ff2274cabda324996e837454d68e096185d3
Edison Pérez <edisonaqp@gmail.com>
2022-09-24 23:42:39 -0500

    modifications

--- Prueba_1/computation.js
+++ Prueba_1/computation.js
@@ -9,8 +9,8 @@
 
 // *** llamando a las funciones que me generan la bolsa de palabras ***
 var mimir = require("./index"),
-  bow = mimir.bow,
-  dict = mimir.dict;
+    bow = mimir.bow,
+    dict = mimir.dict;
 
 console.log("\n---------- Bolsa de Palabras -----------------\n");
 
@@ -22,33 +22,42 @@ const base_text_path = "./base_datos/mails/";
 var filenames = fs.readdirSync(path.join(__dirname, base_text_path));
 
 // *** leyendo la data ***
-var body_mails = [];
-var spam_mails = [];
-for (const file in filenames) {
-  if (filenames[file].slice(0, 5) == "spmsg") {
-    spam_mails.push("Class B"); // es spam = Clase B
-  } else {
-    spam_mails.push("Class A"); // no es spam = Clase A
-  }
-  text = fs
-    .readFileSync(path.join(__dirname, base_text_path + filenames[file]))
-    .toString("utf-8");
-  email_body = text.split("\n")[2]; // la 3ra linea contiene el cuerpo principal del mensaje
-  body_mails.push(email_body);
+var tweets = [];
+var tweet_class = [];
+
+var data = fs.readFileSync("base_datos/training-tweets.csv", "utf-8");
+data = data.split("\r\n");
+for (let i = 0; i < data.length; i++) {
+    data[i] = data[i].split(";");
+    tweets.push(data[i][1]);
+    tweet_class.push(data[i][2]);
 }
+
+// for (const file in filenames) {
+//     if (filenames[file].slice(0, 5) == "spmsg") {
+//         tweet_class.push("Class B"); // es spam = Clase B
+//     } else {
+//         tweet_class.push("Class A"); // no es spam = Clase A
+//     }
+//     text = fs
+//         .readFileSync(path.join(__dirname, base_text_path + filenames[file]))
+//         .toString("utf-8");
+//     email_body = text.split("\n")[2]; // la 3ra linea contiene el cuerpo principal del mensaje
+//     tweets.push(email_body);
+// }
 //console.log(body_mails)
 //console.log(spam_mails)
 
 // *** calculando universo de palabras ***
-var vocabulary = dict(body_mails); //vocabulario
+var vocabulary = dict(tweets); //vocabulario
 //console.log(bow(body_mails[25], vocabulary));
 //console.log(bow(body_mails[25], vocabulary).reduce((a, b) => a + b, 0));
- //console.log("vocabulary: " +  vocabulary);
+//console.log("vocabulary: " +  vocabulary);
 
 // *** calculando la bolsa de palabras (bow) para cada email ***
 var bow_list = [];
-for (const i in body_mails) {
-  bow_list.push(bow(body_mails[i], vocabulary));
+for (const i in tweets) {
+    bow_list.push(bow(tweets[i], vocabulary));
 }
 //console.log(bow_list);
 
@@ -67,12 +76,12 @@ console.log("matrix con transform: " + bow_all_dr);
 
 var all_mails_output_list = [];
 for (i in bow_all_dr) {
-  var one_mail = [];
-  one_mail.push(spam_mails[i]);
-  for (let j = 0; j < new_dimensions; j++) {
-    one_mail.push(bow_all_dr[i][j]);
-  }
-  all_mails_output_list.push(one_mail);
+    var one_mail = [];
+    one_mail.push(tweet_class[i]);
+    for (let j = 0; j < new_dimensions; j++) {
+        one_mail.push(bow_all_dr[i][j]);
+    }
+    all_mails_output_list.push(one_mail);
 }
 
 // ***  Obtenemos la salida final de la lista para el uso en KDTree y KNN
@@ -80,10 +89,10 @@ for (i in bow_all_dr) {
 console.log(all_mails_output_list);
 
 // write JSON string to a file
-const data = JSON.stringify(all_mails_output_list);
-fs.writeFile("final_spam_dr_2d.json", data, (err) => {
-  if (err) {
-    throw err;
-  }
-  console.log("JSON data is saved.");
+const data2 = JSON.stringify(all_mails_output_list);
+fs.writeFile("final_spam_dr_2d.json", data2, (err) => {
+    if (err) {
+        throw err;
+    }
+    console.log("JSON data is saved.");
 });
