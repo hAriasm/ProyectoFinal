26e7b8ebafb6102562807c0e53ae2f10d3ec43f1
Edison Pérez <edisonaqp@gmail.com>
2022-09-25 19:51:39 -0500

    moving code to sketch

--- kdtree/sketch.js
+++ kdtree/sketch.js
@@ -28,25 +28,6 @@ function setup() {
 }
 
 
-function crearCanvasPuntos() {
-  createCanvas();
-  createCanvas(width, height);
-
-  background(0);
-  for (var x = 0; x < width; x += width / scalex) {
-    for (var y = 0; y < height; y += height / scaley) {
-      stroke(125, 125, 125);
-      strokeWeight(1);
-      line(x, 0, x, height);
-      line(0, y, width, y);
-    }
-  }
-
-  for (let i = 0; i < data.length; i++) {
-    drawPoint(data[i]);
-  }
-}
-
 function drawPoint(point, r = 255, g = 255, b = 255) {
   var x = point[0];
   var y = point[1];
@@ -57,37 +38,80 @@ function drawPoint(point, r = 255, g = 255, b = 255) {
   text(x + ", " + y, (x * width) / maxx + 5, height - (y * height) / maxy - 5);
 }
 
-function graficarKNN() {
-  crearCanvasPuntos();
+// function graficarKNN() {
+//   crearCanvasPuntos();
 
-  var cantidadK = document.getElementById("cantidadK").value;
-  var knn = findKnn(root, pointP, parseInt(cantidadK)).nearestNeighbors;
-  console.log("PonitN graf: " + pointP);
+//   var cantidadK = document.getElementById("cantidadK").value;
+//   var knn = findKnn(root, pointP, parseInt(cantidadK)).nearestNeighbors;
+//   console.log("PonitN graf: " + pointP);
 
-  for (let i = 0; i < knn.length; i++) {
-    fill(0, 0, 255);
-    circle(
-      (knn[i].point[0] * width) / maxx,
-      height - (knn[i].point[1] * height) / maxy,
-      10
-    ); 
-    console.log(knn[i].point);
+//   for (let i = 0; i < knn.length; i++) {
+//     fill(0, 0, 255);
+//     circle(
+//       (knn[i].point[0] * width) / maxx,
+//       height - (knn[i].point[1] * height) / maxy,
+//       10
+//     );
+//     console.log(knn[i].point);
+//   }
+
+//   drawPoint(pointP, 0, 255, 0);
+// }
+
+
+function classifier(data, data_test) {
+
+  console.log("cantidad total de entrenamiento: " + data.length);
+
+  var root = build_kdtree(data);
+
+  for (let i = 0; i < data_test.length; i++) {
+    console.log(data_test[i]);
+    knnClassifier(root, [data_test[i][1], data_test[i][2]]);
   }
-
-  drawPoint(pointP, 0, 255, 0);
 }
 
-function limpiarCuadro() {
-  createCanvas(width, height);
-  root = null;
-  background(0);
-  fill(255, 255, 255);
-  for (var x = 0; x < width; x += width / scalex) {
-    for (var y = 0; y < height; y += height / scaley) {
-      stroke(125, 125, 125);
-      strokeWeight(1);
-      line(x, 0, x, height);
-      line(0, y, width, y);
+function knnClassifier(root, pointY) {
+
+  var cantidadK = 1;
+  var knn = findKnn(root, pointY, parseInt(cantidadK)).nearestNeighbors;
+  // console.log(knn);
+  var countHOF = 0, countNOT = 0;
+
+  for (let i = 0; i < knn.length; i++) {
+    // console.log(knn[i].point + ", " + knn[i].value);
+    if (knn[i].value === "HOF") {
+      countHOF++;
+    } else {
+      countNOT++;
     }
   }
+  if (countHOF > countNOT) {
+    console.log("Este tweet es OFENSIVO");
+  } else {
+    console.log("Este tweet NO es OFENSIVO");
+  }
+}
+
+
+function draw() {
+
+  background(0);
+
+  classifier();
+
+  // noFill();
+  // stroke(0, 255, 0);
+  // strokeWeight(4);
+  // rectMode(CENTER);
+  // let range = new Rectangle([mouseX, height - mouseY], [100, 100]);
+  // rect(range.center[0], height - range.center[1], range.scope[0] * 2, range.scope[1] * 2);
+
+  // points = [];
+  // points = range_query_rect(root, range, points);
+  // // console.log("Points found: " + points.length);
+  // for (let p of points) {
+  //   strokeWeight(2);
+  //   drawPoint(p)
+  // }
 }
